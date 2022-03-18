08b1cfe82c861315896474889add83cdad1758ce
TPolito <0613.tyler@gmail.com>
2022-03-17 21:56:02 -0500

    install cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
+		target: '#svelte',
 		adapter: adapter()
 	}
 };
-
-export default config;
