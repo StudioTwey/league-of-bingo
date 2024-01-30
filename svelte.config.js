97ef6567262d726dfa56d27c5be8255b722c008b
Tyler Polito <things@tpolitolcom>
2024-01-30 12:03:43 -0600

    add config

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
+
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
 		preprocess({
-			postcss: true,
-		}),
+			postcss: true
+		})
 	],
 	kit: {
-		adapter: adapter(),
-	},
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
 };
 
 export default config;
