0814679a232686715c7f5b5af2eb32e9446e023e
Tyler Polito <things@tpolitolcom>
2024-01-30 17:31:18 -0600

    refactor board

---
+++ src/lib/components/BingoBoard.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { checkHorizontralBingo, checkVerticalBingo, checkDiagonalBingo } from '$lib/bingo';
+	export let bingoBoard: any[] = [];
+	export let winningBoard;
+
+	function updateBoard(index: number) {
+		bingoBoard[index].selected = !bingoBoard[index].selected;
+		checkBingo(index);
+		localStorage.setItem('storedBoard', JSON.stringify(bingoBoard));
+	}
+
+	function checkBingo(index: number) {
+		let row = Math.floor(index / 5);
+		let col = index % 5;
+
+		let rowBingo = checkHorizontralBingo(bingoBoard, row);
+		let colBingo = checkVerticalBingo(bingoBoard, col);
+		let diaBingo = checkDiagonalBingo(bingoBoard, row, col);
+
+		if (rowBingo || colBingo || diaBingo) {
+			winningBoard = true;
+			return true;
+		}
+		return false;
+	}
+</script>
+
+<div class="grid grid-cols-5">
+	{#each 'PRICE' as letter}
+		<h4 class="text-center text-lg font-bold">{letter}</h4>
+	{/each}
+	{#each bingoBoard as tile}
+		<!-- TODO: FIX THIS -->
+		<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<p
+			class={`border border-black h-[125px] w-[125px] m-0 grid place-items-center text-center overflow-hidden cursor-pointer px-2 text-black ${
+				tile.selected ? 'bg-[#56c9ff]' : 'bg-white'
+			}`}
+			on:click={() => {
+				if (tile.index === 12) return;
+				updateBoard(tile.index);
+			}}
+		>
+			<span>{tile.word}</span>
+		</p>
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import BingoTile from '$lib/components/BingoTile.svelte';
 	import WinningScreen from '$lib/components/WinningScreen.svelte';
-	import {
-		shuffleArray,
-		checkHorizontralBingo,
-		checkVerticalBingo,
-		checkDiagonalBingo
-	} from '$lib/bingo';
+	import { shuffleArray } from '$lib/bingo';
+	import BingoBoard from '$lib/components/BingoBoard.svelte';
 
 	let loading = true;
 	let winningBoard = false;
 	let bingoBoard: any[] = [];
 	let storedBoard: null | string;
+	const inclusiveKeys = ['aubrey', 'kaeman'];
+	const exclusiveKeys = ['league', 'tft', 'soulsLike'];
 
 	async function fetchData() {
 		const res = await fetch(
@@ -28,6 +25,25 @@
 		bingoWords.splice(12, 0, 'FREE');
 
 		return bingoWords;
+	}
+
+	function newBoard() {
+		loading = true;
+		winningBoard = false;
+		bingoBoard = [];
+		localStorage.removeItem('storedBoard');
+
+		fetchData().then((words) => {
+			words.forEach((word: string, index: number) => {
+				bingoBoard.push({
+					word,
+					index,
+					selected: index === 12 ? true : false
+				});
+			});
+			localStorage.setItem('storedBoard', JSON.stringify(bingoBoard));
+			loading = false;
+		});
 	}
 
 	onMount(() => {
@@ -49,67 +65,37 @@
 			loading = false;
 		}
 	});
-
-	function updateBoard(index: number) {
-		bingoBoard[index].selected = !bingoBoard[index].selected;
-		checkBingo(index);
-		localStorage.setItem('storedBoard', JSON.stringify(bingoBoard));
-	}
-
-	function checkBingo(index: number) {
-		let row = Math.floor(index / 5);
-		let col = index % 5;
-
-		let rowBingo = checkHorizontralBingo(bingoBoard, row);
-		let colBingo = checkVerticalBingo(bingoBoard, col);
-		let diaBingo = checkDiagonalBingo(bingoBoard, row, col);
-
-		if (rowBingo || colBingo || diaBingo) {
-			console.log('Bingo');
-			winningBoard = true;
-			return true;
-		}
-		return false;
-	}
-
-	function newBoard() {
-		loading = true;
-		winningBoard = false;
-		bingoBoard = [];
-		localStorage.removeItem('storedBoard');
-
-		fetchData().then((words) => {
-			words.forEach((word: string, index: number) => {
-				bingoBoard.push({
-					word,
-					index,
-					selected: index === 12 ? true : false
-				});
-			});
-			localStorage.setItem('storedBoard', JSON.stringify(bingoBoard));
-			loading = false;
-		});
-	}
 </script>
 
-<div class="flex flex-col items-center content-center absolute text-white m-auto w-full mt-12">
-	<h1 class="text-2xl">League of Bingo</h1>
+<h1 class="text-white text-center text-2xl">League of Bingo</h1>
+<div class="flex flex-col items-center content-center absolute m-auto w-full mt-12">
+	<form class="flex flex-col gap-4" on:submit={newBoard}>
+		<p class="text-white">Select a game</p>
+		<select>
+			{#each exclusiveKeys as key}
+				<option class="text-black" value={key}>{key}</option>
+			{/each}
+		</select>
+		<div>
+			{#each inclusiveKeys as key}
+				<label for={key} class="text-white">
+					<input type="checkbox" id={key} name={key} value={key} />
+					{key}
+				</label>
+			{/each}
+		</div>
+		<button class="border-white border-2 text-black bg-white outline-none" type="submit"
+			>New Board</button
+		>
+	</form>
+
 	{#if loading}
 		<p>Loading...</p>
 	{:else}
-		{#if winningBoard}
-			<WinningScreen />
-		{/if}
-
-		<div class="grid grid-cols-5">
-			{#each 'PRICE' as letter}
-				<h4 class="text-center text-lg font-bold">{letter}</h4>
-			{/each}
-			{#each bingoBoard as tile}
-				<BingoTile {tile} {updateBoard} />
-			{/each}
-		</div>
-		<!-- TODO: Style this -->
-		<button class="mt-4" on:click={newBoard}>New Board</button>
+		<BingoBoard {winningBoard} {bingoBoard} />
 	{/if}
 </div>
+
+{#if winningBoard}
+	<WinningScreen />
+{/if}
